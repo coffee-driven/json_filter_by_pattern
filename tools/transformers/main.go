95b32b195ed4f3c3f66decd81d11430a40aa9b28
m <coffee_driven>
2023-06-30 10:33:30 +0200

    Implement logger

--- tools/transformers/main.go
+++ tools/transformers/main.go
@@ -3,11 +3,32 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"os"
 
 	"go.uber.org/zap"
 )
+
+type Logger interface {
+	Debug(args ...interface{})
+	Error(args ...interface{})
+	Info(args ...interface{})
+	Warn(args ...interface{})
+}
+
+type ZapLogger struct {
+	logger *zap.SugaredLogger
+}
+
+func NewLogger() *ZapLogger {
+	new_logger, err := zap.NewProduction()
+	if err != nil {
+		fmt.Println("Couldn't initialize zap logging")
+	}
+	sugar := new_logger.Sugar()
+	sugar.Debug("Logger initialized")
+
+	return &ZapLogger{logger: sugar}
+}
 
 type Patterns struct {
 	NamePattern        string `json:"name_pattern"`
@@ -19,13 +40,15 @@ type Product struct {
 	Product_name        string
 	Product_price       float64
 	Product_description string
+	logger              Logger
 }
 
-func NewProduct() *Product {
+func NewProduct(logger Logger) *Product {
 	p := &Product{
 		Product_name:        "name",
 		Product_price:       000000,
 		Product_description: "description",
+		logger:              logger,
 	}
 
 	return p
@@ -36,14 +59,26 @@ func (p *Product) FilterJsonToProductByPatterns(json_data map[string]interface{}
 
 	for key, value := range json_data {
 		switch key {
-		case patterns.NamePattern:
-			p.Product_name = value.(string)
 		case patterns.DescriptionPattern:
-			p.Product_description = value.(string)
+			if v, ok := value.(string); !ok {
+				p.logger.Warn("Invalid description type. Must be strong. Maybe missing quotes?")
+			} else {
+				p.Product_description = v
+			}
+		case patterns.NamePattern:
+			if v, ok := value.(string); !ok {
+				p.logger.Warn("Invalid name type. Must be string. Maybe missing quotes?")
+			} else {
+				p.Product_name = v
+			}
 		case patterns.PricePattern:
-			p.Product_price = value.(float64)
+			if v, ok := value.(float64); !ok {
+				p.logger.Warn("Invalid price type. Must be float. Maybe ambitious quotes?")
+			} else {
+				p.Product_price = v
+			}
 		default:
-			fmt.Printf("Key not matching any of the patterns")
+			p.logger.Debug("Key not matching any of the patterns")
 
 		}
 	}
@@ -53,13 +88,16 @@ func (p *Product) FilterJsonToProductByPatterns(json_data map[string]interface{}
 
 type ProductList struct {
 	products []Product
+	logger   Logger
 }
 
-func NewProductList() *ProductList {
+func NewProductList(logger Logger) *ProductList {
+	logger.Debug("Init new product list")
 	var p []Product
 
 	pl := &ProductList{
 		products: p,
+		logger:   logger,
 	}
 
 	return pl
@@ -69,7 +107,7 @@ func (pl *ProductList) UpdateProductList(json_data []map[string]interface{},
 	search_patterns Patterns) *ProductList {
 
 	for _, product := range json_data {
-		p := NewProduct()
+		p := NewProduct(pl.logger)
 		product := p.FilterJsonToProductByPatterns(product, search_patterns)
 
 		pl.products = append(pl.products, *product)
@@ -81,14 +119,9 @@ func (pl *ProductList) UpdateProductList(json_data []map[string]interface{},
 
 func main() {
 
-	logger, err := zap.NewProduction()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	sugar := logger.Sugar()
-
-	sugar.Info("Hello from zap logger")
+	logger := NewLogger()
+	log := logger.logger
+	log.Info("logger started")
 
 	// Load patterns
 	var patterns_struct Patterns
@@ -96,27 +129,18 @@ func main() {
 
 	patterns, err := os.ReadFile(patterns_file)
 	if err != nil {
-		sugar.Error("Error reading pattern file")
+		log.Error("Error reading pattern file")
 	}
 
 	if err := json.Unmarshal(patterns, &patterns_struct); err != nil {
-		sugar.Error("Error unmarshaling pattern file")
+		log.Error("Error unmarshal pattern file")
 	}
-
-	/*
-		my_patterns := make(map[string]string)
-		my_patterns["Product_name"] = "Name"
-		my_patterns["Product_price"] = "PSPrice"
-		my_patterns["Product_description"] = "Notes"
-
-		patterns := sp.newSearchPatterns(my_patterns)
-	*/
 
 	// Load data
 	var file_name string = "data.json"
 	jsonData, err := os.ReadFile(file_name)
 	if err != nil {
-		sugar.Error("Couldn't read file: %s", file_name)
+		log.Error("Couldn't read file: %s", file_name)
 	}
 	var data []map[string]interface{}
 	if err := json.Unmarshal(jsonData, &data); err != nil {
@@ -124,7 +148,7 @@ func main() {
 	}
 
 	// Create convention product list
-	pl := NewProductList()
+	pl := NewProductList(log)
 	product_list := pl.UpdateProductList(data, patterns_struct)
 
 	fmt.Println(product_list)
